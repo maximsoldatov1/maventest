655ba11643d10a586032a8fbd37f0faa3b7ecc09
Maxim Soldatov <maxim.soldatov@weigandt-consulting.com>
2016-03-03 06:35:54 -0500

    2nd line modified

--- src/main/java/com/weigandtconsulting/app/App.java
+++ src/main/java/com/weigandtconsulting/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Another line" );
+        System.out.println( "Changed 2nd line" );
     }
 }
